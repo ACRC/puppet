b0a19b2fbca3a0b34cf59d2df0974ea9a10dcab4
Dan Foster <dan.foster@bristol.ac.uk>
2014-09-03 10:15:06 +0100

    Move roles to class

---
+++ modules/role/manifests/base.pp
@@ -0,0 +1,4 @@
+class role::base {
+    include motd
+    include puppet
+}
---
+++ modules/role/manifests/puppetmaster.pp
@@ -0,0 +1,18 @@
+class role::puppetmaster() {
+    include acrc_puppet::master
+    include foreman
+    include foreman_proxy
+
+
+    class { 'puppetdb': }
+    class { 'puppetdb::master::config':
+        manage_storeconfigs => false
+    }
+    
+    
+    postgresql::server::db { $foreman::db_database:
+        user     => $foreman::db_username,
+        password => $foreman::db_password,
+        owner    => $foreman::db_username,
+    }
+}
